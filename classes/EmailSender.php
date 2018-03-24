eb1752cabdc009b862d3a3a4d4afc1da268c3b27
VirtusDesktopPC <vbo@rastec.dk>
2018-03-24 07:10:17 +0100

    hg

--- classes/EmailSender.php
+++ classes/EmailSender.php
@@ -5,6 +5,78 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 class EmailSender
 {
+    public function contactFormSend()
+    {
+        //Load Composer's autoloader
+        require 'vendor/autoload.php';
+
+        date_default_timezone_set('Europe/Copenhagen');
+
+        // admin@dating.virtusb.com, kode: rootpwdating13
+
+        $mail = new PHPMailer(true); // Passing `true` enables exceptions
+        $mail->CharSet = 'UTF-8';
+        try {
+            //Server settings
+            $mail->SMTPDebug = 0; // Enable verbose debug output
+            $mail->isSMTP(); // Set mailer to use SMTP
+            $mail->Host = 'de9.fcomet.com'; // Specify main and backup SMTP servers
+            $mail->SMTPAuth = true; // Enable SMTP authentication
+            $mail->Username = 'admin@dating.virtusb.com'; // SMTP username
+            $mail->Password = 'rootpwdating13'; // SMTP password
+            $mail->SMTPSecure = 'ssl'; //false;                            // Enable TLS encryption, `ssl` also accepted
+            $mail->Port = 465; //587; //465;                                    // TCP port to connect to
+
+            //Recipients
+            $mail->setFrom('admin@dating.virtusb.com', 'TEC DatingApp');
+            $mail->addAddress('admin@dating.virtusb.com'); // Add a recipient
+            $mail->addReplyTo('admin@dating.virtusb.com', 'TEC DatingAp');
+
+            //Attachments
+            //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+            function getUserIpAddr()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))
+    {
+        return $_SERVER['HTTP_CLIENT_IP'];
+    }
+    else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    {
+        return $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+        return $_SERVER['REMOTE_ADDR'];
+    }
+}
+
+            //Content
+            $mail->isHTML(true); // Set email format to HTML
+            $mail->Subject = 'Besked fra besøgende på dating.virtusb.com';
+            $mail->Body = "<p style='font-size: 30px;'><strong>Besked:</strong></p> " . $_POST['contact-message'] .
+
+              "<p style='font-size: 30px;'><strong>Afsender navn:</strong></p> " . $_POST['contact-firstname'] . " " . $_POST['contact-lastname'] .
+
+              "<p style='font-size: 30px;'><strong>Afsender email:</strong></p> " . $_POST['contact-email'] .
+
+              "<p style='font-size: 30px;'><strong>Afsender tlf. nr:</strong></p> " . $_POST['contact-phone'] .
+
+              //"<p style='font-size: 30px;'><strong>Foretrækker at blive kontaktet om:</strong></p> " . $_POST['contact-preference'] .
+
+              "<p style='font-size: 30px;'><strong>IP adresse:</strong></p> " . getUserIpAddr();
+            //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            return true;
+            //echo 'Message has been sent';
+        } catch (Exception $e) {
+            //echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+        }
+
+    }
+
     public function sendWelcomeEmail()
     {
 
@@ -16,6 +88,7 @@ class EmailSender
         // admin@dating.virtusb.com, kode: rootpwdating13
 
         $mail = new PHPMailer(true); // Passing `true` enables exceptions
+        $mail->CharSet = 'UTF-8';
         try {
             //Server settings
             $mail->SMTPDebug = 0; // Enable verbose debug output
@@ -39,7 +112,14 @@ class EmailSender
             //Content
             $mail->isHTML(true); // Set email format to HTML
             $mail->Subject = 'Din konto hos DatingApp er blevet oprettet';
-            $mail->Body = 'Velkommen, ' . strtok($_POST['name'], " ");
+            $mail->Body = "<p style='font-size: 30px;'>Velkommen, " . ucfirst(strtolower(strtok($_POST['name'], " "))) . "</p>" .
+            "<br><p style='font-size: 30px;'>Her er dine kontodetaljer</p>" .
+            "<p>Fulde navn: " . ucfirst(strtolower($_POST['name'])) . "</p>" .
+                "<p>Email: " . $_POST['email'] . "</p>" .
+                "<p>Brugernavn: " . $_POST['username'] . "</p>" .
+                "<p>Adgangskode: " . $_POST['password'] . "</p>" .
+                "<br><br><p style='font-size: 24px;'>Vi håber du møder den du søger!</p>" .
+                "<br><p>&copy; 2018 Dating App</p>";
             //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
             $mail->send();
--- kontakt.php
+++ kontakt.php
@@ -2,13 +2,141 @@
 require_once 'core/init.php';
 include 'includes/components/header.php';
 
+
+    
+if (!empty($_POST["contact-firstname"]) && !empty($_POST["contact-lastname"]) && !empty($_POST["contact-email"]) && !empty($_POST["contact-message"])) {
+    $EmailSender = new EmailSender();
+    //$EmailSender->contactFormSend();
+    if($EmailSender->contactFormSend()) {
+        Session::flash('home', 'Din besked blev sendt');
+        Redirect::to('forside');
+    } else {
+        Session::flash('home', 'Fejl. Prøv at sende mailen manuelt til admin@dating.virtusb.com');
+        Redirect::to('forside');
+    }
+}
+
 echo '<main>';
-
-
 ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col m10 offset-m1 s12">
+            <h2 style="color: #535151" class="center-align">Hvad kan vi hjælpe med?</h2>
+            <div class="row">
+                <form method="post" id="contact-form" class="col s12">
+                    <div class="row">
+                        <div class="input-field col m6 s12">
+                            <input id="first_name" name="contact-firstname" type="text" class="validate">
+                            <label for="first_name">Fornavn</label>
+                        </div>
+                        <div class="input-field col m6 s12">
+                            <input id="last_name" name="contact-lastname" type="text" class="validate">
+                            <label for="last_name">Efternavn</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col m6 s12">
+                            <i class="mdi-content-mail prefix"></i>
+                            <input id="email" name="contact-email" type="text" class="validate">
+                            <label for="email">Email</label>
+                        </div>
+                        <div class="input-field col m6 s12">
+                            <i class="mdi-maps-store-mall-directory prefix"></i>
+                            <input id="phone_number" name="contact-phone" type="tel" class="validate">
+                            <label for="phone_number">Telefon nr.</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                          <textarea id="message" name="contact-message" class="materialize-textarea"></textarea>
+                          <label for="message">Besked</label>
+                        </div>
+                    </div>
+
+                   
+
+                    <div class="row">
+                        <div class="input-field col m6 s12">
+                            <i class="mdi-maps-store-mall-directory prefix"></i>
+                            <input id="vertification" type="text" class="validate">
+                            <label for="vertification">Hvad er 2 +2?</label>
+                        </div>
+                        <div class="col m6">
+                         <p class="right-align"><button id="contact-submit" class="btn btn-large waves-effect waves-light" type="submit" name="contact-submit">Send Besked</button></p>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
  </main>
 
 <?php include 'includes/components/footer.php'; ?>
+<script>
+$(document).ready(function() {
+  $("#contact-submit").on("click", function() {
+    errorMessage = "";
+    var contactFormValid = true;
+    if ($("#first_name").val() == "") {
+      errorMessage = "Du er nød til at indtaste et fornavn";
+      contactFormValid = false;
+      $("#contact-form").submit(function(e) {
+        e.preventDefault();
+      });
+    } else if($('#last_name').val() == "") {
+      errorMessage = "Du er nød til at indtaste et efternavn";
+      contactFormValid = false;
+      $("#contact-form").submit(function(e) {
+        e.preventDefault();
+      });
+    } else if($('#phone_number').val() == "") {
+      errorMessage = "Du er nød til at indtaste et telefon nr.";
+      contactFormValid = false;
+      $("#contact-form").submit(function(e) {
+        e.preventDefault();
+      });
+    } else if($('#email').val() == "") {
+      errorMessage = "Du er nød til at indtaste en email";
+      contactFormValid = false;
+      $("#contact-form").submit(function(e) {
+        e.preventDefault();
+      });
+    } else if(/\S+@\S+\.\S+/.test($('#email').val()) == false) {
+      errorMessage = "Du er nød til at indtaste en korrekt email adresse";
+      contactFormValid = false;
+      $("#contact-form").submit(function(e) {
+        e.preventDefault();
+      });
+    } else if($('#message').val() == "") {
+      errorMessage = "Du er nød til at indtaste en besked";
+      contactFormValid = false;
+      $("#contact-form").submit(function(e) {
+        e.preventDefault();
+      });
+    } else if($('#vertification').val() != 4) {
+      errorMessage = "Svaret givet til 2 + 2 er forkert";
+      contactFormValid = false;
+      $("#contact-form").submit(function(e) {
+        e.preventDefault();
+      });
+    } 
+    if (!contactFormValid && errorMessage.length > 0) {
+      alertify.alert('Fejl', errorMessage, function() {
+        alertify.message("OK");
+      });
+    } else {
+      $("#contact-form")
+        .unbind("submit")
+        .submit();
+    }
+  });
+});
+</script>
 
 </body>
 </html>
